99cdf923924e75ab535b9315495fb1884849c303
Victor Mours <victor.mours@tigerlilyapps.com>
2014-04-08 14:19:53 +0200

    Add basic tweaks

--- kinesis_remaps.md
+++ kinesis_remaps.md
@@ -5,7 +5,14 @@ Key -> Function
 
 Delete -> Cmd
 BackSpace -> Space
+PageUp -> Alt
 PageDown -> Delete
 
+Up Arrow -> Down Arrow
+Down Arrow -> Up Arrow
 
 
+
+Macros
+
+Ctr - Cmd - Space -> F9
---
+++ mac_os_tweaks.sh
@@ -0,0 +1,5 @@
+# Faster mouse key
+defaults write com.apple.universalaccess "mouseDriverInitialDelay" 0.05
+
+# Key remaps
+~/Applications/PCKeyboardHack.app/Contents/Library/bin/PCKeyboardHack_cliset keycode_capslock 53
