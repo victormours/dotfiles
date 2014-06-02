f43756b82d162fe3fb733b12ba2698a0d156a4f9
Victor Mours <victor.mours@tigerlilyapps.com>
2014-06-02 10:16:30 +0200

    improve aliases

--- aliases.sh
+++ aliases.sh
@@ -40,6 +40,7 @@ explicit_alias gac 'git add -A .; git commit -m'
 explicit_alias ga 'git add -A'
 explicit_alias gap 'git add --patch'
 explicit_alias gcm 'git commit -m'
+explicit_alias gci 'git commit'
 
 explicit_alias gps 'git push'
 
@@ -58,6 +59,8 @@ explicit_alias grim 'git rebase -i master'
 explicit_alias grid 'git rebase -i development'
 
 # Other git aliases
+explicit_alias gdd 'git diff development'
+explicit_alias gdm 'git diff master'
 explicit_alias gi 'vim .gitignore'
 explicit_alias gcp 'git cp'
 explicit_alias gm 'git merge'
@@ -95,13 +98,17 @@ explicit_alias 'front' 'cd ~/code/monitoring; mvim'
 explicit_alias 'back' 'cd ~/code/monitoring-api; mvim'
 explicit_alias 'bootstrap' 'cd ~/code/tigerlily-bootstrap; mvim'
 explicit_alias 'blog' 'cd ~/code/tigerlily.github.io; mvim'
+explicit_alias 'perf' 'cd ~/dev/perf; mvim'
 
 # Moving to projects
 explicit_alias 'code' 'cd ~/code'
 explicit_alias 'cdfront' 'cd ~/code/monitoring'
 explicit_alias 'cdback' 'cd ~/code/monitoring-api'
 explicit_alias 'cddotfiles' 'cd ~/dev/dotfiles'
+explicit_alias 'cdblog' 'cd ~/code/tigerlily.github.io'
 
+explicit_alias 'brep' 'open https://github.com/tigerlily/monitoring-api'
+explicit_alias 'frep' 'open https://github.com/tigerlily/monitoring'
 explicit_alias 'bpr' 'open https://github.com/tigerlily/monitoring-api/pulls'
 explicit_alias 'fpr' 'open https://github.com/tigerlily/monitoring/pulls'
 explicit_alias 'twitterpr' 'open https://github.com/tigerlily/twitterlily/pulls'
@@ -110,6 +117,7 @@ explicit_alias 'bootstrappr' 'open https://github.com/tigerlily/tigerlily-bootst
 # SSH
 explicit_alias 'sshwallmanager' 'ssh deploy@factory1.tigerlilyplatform.com'
 explicit_alias 'sshtwitterlilyprod' 'ssh deploy@twitterlily.tigerlilyplatform.com'
+explicit_alias 'sshtwitterlilystg' 'ssh deploy@twitterlily.staging.tigerlilyplatform.com'
 explicit_alias 'sshsiprod' 'ssh deploy@socialinbox.tigerlilyplatform.com'
 explicit_alias 'sshsistg' 'ssh deploy@socialinbox.staging.tigerlilyplatform.com'
 
