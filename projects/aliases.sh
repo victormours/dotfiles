3b16eccadd2102eddb288c80cfb3211863b09448
Victor Mours <victor.mours@tigerlilyapps.com>
2014-06-16 16:45:13 +0200

    add wpr shortcut

--- projects/aliases.sh
+++ projects/aliases.sh
@@ -27,6 +27,7 @@ explicit_alias 'brep' 'open https://github.com/tigerlily/monitoring-api'
 explicit_alias 'frep' 'open https://github.com/tigerlily/monitoring'
 explicit_alias 'bpr' 'open https://github.com/tigerlily/monitoring-api/pulls'
 explicit_alias 'fpr' 'open https://github.com/tigerlily/monitoring/pulls'
+explicit_alias 'wpr' 'open https://github.com/wallmanager/monitoring/pulls'
 explicit_alias 'twitterpr' 'open https://github.com/tigerlily/twitterlily/pulls'
 explicit_alias 'bootstrappr' 'open https://github.com/tigerlily/tigerlily-bootstrap/pulls'
 
