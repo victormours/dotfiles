101b248cc38693d1a60b198f59ad36437d85d0e2
Victor Mours <victor.mours@gmail.com>
2014-06-25 23:52:37 +0200

    Add basic prompt

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ chsh -s $(which zsh)
 #Symlink .zshrc and .vimrc
 ln -s $HOME/dev/dotfiles/vim/vimrc $HOME/.vimrc
 ln -s $HOME/dev/dotfiles/zsh/zshrc $HOME/.zshrc
+
+# Install Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
