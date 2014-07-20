92a7b619cb9aba9bf50730a51aa02508c57ee142
Victor Mours <victor.mours@gmail.com>
2014-07-20 22:22:46 +0200

    install autojump

--- install.sh
+++ install.sh
@@ -5,5 +5,13 @@ chsh -s $(which zsh)
 ln -s $HOME/dev/dotfiles/vim/vimrc $HOME/.vimrc
 ln -s $HOME/dev/dotfiles/zsh/zshrc $HOME/.zshrc
 
+# Install Autojump
+git clone git://github.com/joelthelion/autojump.git ~/.dotfiles/autojump
+~/.dotfiles/autojump/install.py
+
+
 # Install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+
