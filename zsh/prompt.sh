7a60a594cc3244e5c91981ab5cd4b90712df16d8
Victor Mours <victor.mours@tigerlilyapps.com>
2014-09-12 15:35:17 +0200

    Add git explicit git status

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,7 +1,26 @@
+# Heavily inspired form skwp's prompt
+
 function set_git_prompt {
   if git status &> /dev/null ; then
     branchname=`git rev-parse --abbrev-ref HEAD`
-    git_prompt="(%{$branchname%}) "
+    git_status=`git status --porcelain | awk '{print $1}'`
+
+    git_added=`echo $git_status | grep A`
+    git_modified=`echo $git_status | grep M`
+    git_untracked=`echo $git_status | grep \?\?`
+
+    git_status_prompt=''
+    if [ -n "$git_added" ]; then
+      git_status_prompt+='%{%F{green}%}●%{%f%}'
+    fi
+    if [ -n "$git_modified" ]; then
+      git_status_prompt+='%{%F{yellow}%}●%{%f%}'
+    fi
+    if [ -n "$git_untracked" ]; then
+      git_status_prompt+='%{%F{red}%}●%{%f%}'
+    fi
+
+    git_prompt="(%{$branchname%}%{$git_status_prompt%}) "
   else
     git_prompt=''
   fi
