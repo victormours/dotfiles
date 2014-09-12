b9440a47922adadb84fb4d61a7fb50b7039315ab
Victor Mours <victor.mours@tigerlilyapps.com>
2014-09-12 14:54:05 +0200

    Set git prompt using precmd_functions'

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,4 +1,4 @@
-function precmd {
+function set_git_prompt {
   if git status &> /dev/null ; then
     branchname=`git rev-parse --abbrev-ref HEAD`
     git_prompt="(%{$branchname%}) "
@@ -7,11 +7,11 @@ function precmd {
   fi
 }
 
-setopt prompt_subst
+precmd_functions+=set_git_prompt
 
 user_prompt='%{%F{magenta}%}%n%{%F{default}%}@%{%F{yellow}%}%m%{%F{default}%}'
 path_prompt='%{%F{green}%}%1~%{%F{default}%}'
 
+setopt prompt_subst
 PROMPT='%{$user_prompt%} %{$path_prompt%} %{$git_prompt%}%# '
-
 
