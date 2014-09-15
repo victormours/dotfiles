5588f2918bbc893b37e90514beb2fb5234f039f5
Victor Mours <victor.mours@tigerlilyapps.com>
2014-09-15 12:11:49 +0200

    Add basic gitconfig

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ chsh -s $(which zsh)
 #Symlink .zshrc and .vimrc
 ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
 
 # Install Autojump
 git clone git://github.com/joelthelion/autojump.git ~/.dotfiles/autojump
