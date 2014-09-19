081ec6a82ee5d8bdd9cf4f2ddab0e24ec67c4991
Victor Mours <victor.mours@tigerlilyapps.com>
2014-09-19 11:32:01 +0200

    Add deploy_current_branch function

--- ruby/utils.sh
+++ ruby/utils.sh
@@ -3,12 +3,16 @@ function cap_deploy_branch {
   bundle exec cap staging deploy -S branch=$branchname
 }
 
-alias cdb=cap_deploy_branch
+function deploy_current_branch {
+  cap_deploy_branch `git rev-parse --abbrev-ref HEAD`
+}
 
 function open_gem {
   gem_name=$1
   mvim `bundle show $gem_name`
 }
 
+alias cdb=cap_deploy_branch
+alias dcb=deploy_current_branch
 alias og=open_gem
 
