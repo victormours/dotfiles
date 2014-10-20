406c6884d3edf1b2bed82ad4cc3daaa5252ec8e0
Victor Mours <victor.mours@tigerlilyapps.com>
2014-10-20 11:32:10 +0200

    better alias for bootstrap pr

--- projects/aliases.sh
+++ projects/aliases.sh
@@ -3,9 +3,9 @@ explicit_alias 'frep' 'open https://github.com/tigerlily/monitoring'
 explicit_alias 'bpr' 'open https://github.com/tigerlily/monitoring-api/pulls'
 explicit_alias 'spr' 'open https://github.com/tigerlily/monitoring/pulls'
 explicit_alias 'fpr' 'open https://github.com/tigerlily/fetcher/pulls'
+explicit_alias 'bootpr' 'open https://github.com/tigerlily/tigerlily-bootstrap/pulls'
 explicit_alias 'wpr' 'open https://github.com/tigerlily/wallmanager/pulls'
 explicit_alias 'twitterpr' 'open https://github.com/tigerlily/twitterlily/pulls'
-explicit_alias 'bootstrappr' 'open https://github.com/tigerlily/tigerlily-bootstrap/pulls'
 
 explicit_alias sleek 'open https://socialinbox.tigerlilyplatform.com'
 
