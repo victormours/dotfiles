3cea9c24a6e45e136fed5682e6d882940ff8898d
Victor Mours <victor.mours@gmail.com>
2014-11-23 11:32:42 +0100

    Add bec alias

--- ruby/aliases.sh
+++ ruby/aliases.sh
@@ -2,6 +2,7 @@ explicit_alias b 'bundle'
 explicit_alias be 'bundle exec'
 
 alias rs='bundle exec rspec'
+alias bec='bundle exec cucumber'
 explicit_alias rp 'bundle exec rspec-preloader'
 
 # deploying
