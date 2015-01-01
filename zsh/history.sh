91a3d436efaa200cf0125015ac91c78388583712
Victor Mours <victor.mours@gmail.com>
2015-01-01 19:39:37 +0100

    Improve zsh history Options taken from github.com/robbyrussel/oh-my-zsh

--- zsh/history.sh
+++ zsh/history.sh
@@ -1,3 +1,15 @@
-HISTFILE=~/.zsh_history
-HISTSIZE=1000
-SAVEHIST=1000
+## Command history configuration
+if [ -z "$HISTFILE" ]; then
+    HISTFILE=$HOME/.zsh_history
+fi
+
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history # share command history data
