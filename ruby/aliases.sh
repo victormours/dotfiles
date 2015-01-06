b400f79f66752782f5ec86e7a6f7389a4b794251
Victor Mours <victor.mours@tigerlilyapps.com>
2015-01-06 11:29:47 +0100

    improve rspec aliases

--- ruby/aliases.sh
+++ ruby/aliases.sh
@@ -2,8 +2,11 @@ alias bi='bundle install'
 alias bu='bundle update'
 
 alias rs='bundle exec rspec'
+alias rp='bundle exec rspec-preloader'
+alias rps='bundle exec rspec-preloader-server'
+alias rpc='bundle exec rspec-preloader-client'
+
 alias bec='bundle exec cucumber'
-explicit_alias rp 'bundle exec rspec-preloader'
 
 # deploying
 alias csd='bundle exec cap sandbox deploy'
