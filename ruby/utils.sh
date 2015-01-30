5cdada03fd65bc6871fde95d2983a7d9bb5d533d
Victor Mours <victor.mours@tigerlilyapps.com>
2015-01-30 17:57:33 +0100

    Improve ruby aliases and facilitate usage of cap remote

--- ruby/utils.sh
+++ ruby/utils.sh
@@ -2,17 +2,28 @@ function cap_deploy_branch {
   branchname=$1
   bundle exec cap sandbox deploy -S branch=$branchname
 }
+alias cdb=cap_deploy_branch
 
 function deploy_current_branch {
   cap_deploy_branch `git rev-parse --abbrev-ref HEAD`
 }
+alias dcb=deploy_current_branch
 
 function open_gem {
   gem_name=$1
   (cd `bundle show $gem_name` && mvim .)
 }
-
-alias cdb=cap_deploy_branch
-alias dcb=deploy_current_branch
 alias og=open_gem
+
+function remote_shell {
+  environment=$1
+  cap $environment remote:shell
+}
+alias resh=remote_shell
+
+function remote_console {
+  environment=$1
+  cap $environment remote:console
+}
+alias rc=remote_console
 
