21e6624acddaa5411edade1da789a2890e9197c4
Victor Mours <victor.mours@tigerlilyapps.com>
2015-01-30 18:10:08 +0100

    Refactor top_commands functions

--- zsh/history.sh
+++ zsh/history.sh
@@ -14,18 +14,18 @@ setopt hist_verify
 setopt inc_append_history
 
 function top_commands {
-  cat ~/.zsh_history|cut -d ';' -f 2- 2>/dev/null |
-    awk '{a[$1]++ } END{for(i in a){print a[i] " " i}}'|
-    sort -rn |
-    head -n20
+  cat ~/.zsh_history | format_history
 }
 alias tc=top_commands
 
 function top_recent_commands {
-  tail -n1000 ~/.zsh_history|cut -d ';' -f 2- 2>/dev/null |
+  tail -n1000 ~/.zsh_history | format_history
+}
+alias trc=top_recent_commands
+
+function format_history {
+  cut -d ';' -f 2- 2>/dev/null |
     awk '{a[$1]++ } END{for(i in a){print a[i] " " i}}'|
     sort -rn |
     head -n20
 }
-alias trc=top_recent_commands
-
