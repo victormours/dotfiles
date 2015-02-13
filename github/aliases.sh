925cee8d11da7e6c624ce3e83df7c9db312b24e8
Victor Mours <victor.mours@tigerlilyapps.com>
2015-02-13 11:30:35 +0100

    gmm and prs aliases

--- github/aliases.sh
+++ github/aliases.sh
@@ -1,3 +1,3 @@
 alias h=hub
-alias pulls='hub browse -- pulls'
+alias prs='hub browse -- pulls'
 alias repo='hub browse'
--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,7 +7,6 @@ alias l='less -r'
 alias psg='ps aux | grep'
 
 # Custom utils
-alias prs='touch tmp/restart.txt; echo "Pow restarted"'
 alias gruntt='grunt test'
 alias gubu='gulp build'
 
