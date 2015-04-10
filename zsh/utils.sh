bf53c27e81b8f2da22de5759e182e1ddc9eb5af8
Victor Mours <victor.mours@tigerlilyapps.com>
2015-04-10 18:54:51 +0200

    Remove non working utils

--- zsh/utils.sh
+++ zsh/utils.sh
@@ -1,17 +1,3 @@
-function open_project {
-  project_name=$1
-  autojump $project_name
-  mvim
-}
-
-alias o=open_project
-
-function md {
-  directory_name=$1
-  mkdir $1
-  cd $1
-}
-
 source $HOME/.dotfiles/ruby/utils.sh
 source $HOME/.dotfiles/git/utils.sh
 
