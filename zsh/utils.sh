fd611f456710223ed8aa8cdf13124dd1fd63da79
Victor Mours <victor.mours@gmail.com>
2015-04-12 16:29:41 +0200

    Open applications from the shell This is faster than using cmd-tab

--- zsh/utils.sh
+++ zsh/utils.sh
@@ -1,3 +1,6 @@
 source $HOME/.dotfiles/ruby/utils.sh
 source $HOME/.dotfiles/git/utils.sh
 
+alias m="osascript -e 'tell application \"MacVim\" to activate'"
+alias h="osascript -e 'tell application \"Chrome\" to activate'"
+alias k="osascript -e 'tell application \"Slack\" to activate'"
