6fbaab62e2ea98f25a548e7ef2bfb1d201ec43a9
Victor Mours <victor.mours@tigerlilyapps.com>
2015-04-20 19:29:04 +0200

    fix

--- zsh/mandatory_aliases.sh
+++ zsh/mandatory_aliases.sh
@@ -26,6 +26,6 @@ bindkey '^J' check-alias-and-accept
 bindkey '^M' check-alias-and-accept
 
 function zle-line-finish {
-  region_highlight+=("0 $#BUFFER fg=green")
+  region_highlight+=("0 $#BUFFER fg=blue")
 }
 zle -N zle-line-finish
