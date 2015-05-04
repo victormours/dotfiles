ad544534cec2a62f12177238ce8f83da059855c3
Haroen Viaene <fingebimus@me.com>
2015-05-04 13:06:01 +0200

    autojump is on homebrew too

--- install.sh
+++ install.sh
@@ -8,11 +8,6 @@ ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
 
-# Install Autojump
-git clone git://github.com/joelthelion/autojump.git ~/.dotfiles/autojump
-(cd ~/.dotfiles/autojump/ && ./install.py)
-
-
 # Install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -21,6 +16,7 @@ vim +BundleInstall +q
 # Brew specific installs
 brew install the_silver_searcher
 brew install rbenv-bundler
+brew install autojump
 
 rbenv bundler on
 
