03bc7c2971e6d5306e7ea09c3fc8001ba6328452
Victor Mours <victor.mours@gmail.com>
2015-05-05 00:51:54 +0200

    Add ip alias by @duivv

--- zsh/utils.sh
+++ zsh/utils.sh
@@ -7,3 +7,5 @@ alias h="osascript -e 'tell application \"Chrome\" to activate'"
 alias k="osascript -e 'tell application \"Slack\" to activate'"
 
 alias "review"="open 'https://github.com/pulls?q=is%3Aopen+is%3Apr+label%3A%22Needs+Review%22+user%3Atigerlily+sort%3Acreated-asc'"
+
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
