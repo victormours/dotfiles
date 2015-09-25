bfdd22deec9ead81542b6c55864f17012d4df1b8
Victor Mours <victor.mours@tigerlilyapps.com>
2015-09-25 14:46:25 +0200

    Remove merged remote branches when git cleaning

--- git/utils.sh
+++ git/utils.sh
@@ -3,7 +3,7 @@ function git_delete_branch_for_real {
 }
 
 function git_clean_branches {
-  git branch --merged master | grep -v "master" | grep -v "^\*" | xargs -n 1 git branch -d
+  git branch --merged master | grep -v "master" | grep -v "^\*" | xargs -n 1  bash -c 'git branch -D $@ && git push origin $@ 2> /dev/null' bash
 }
 
 alias gcl=git_clean_branches
