803a25e6e4c3ee09e38d3665abb0ce1931dbd586
Victor Mours <victor.mours@tigerlilyapps.com>
2015-09-25 10:46:58 +0200

    Add git_delete_branch_for_real Taken from http://drewbarontini.com/articles/5-command-line-productivity-tips

--- git/utils.sh
+++ git/utils.sh
@@ -1,3 +1,7 @@
+function git_delete_branch_for_real {
+  git branch -D $1 && git push origin :$1
+}
+
 function git_clean_branches {
   git branch --merged master | grep -v "master" | grep -v "^\*" | xargs -n 1 git branch -d
 }
