21f8cd8760dfa851cc27e675f605d5a26c570f87
Victor Mours <victor.mours@gmail.com>
2015-10-15 18:49:16 +0200

    Add a decent gemrc (stolen from https://github.com/r00k/dotfiles/blob/master/gemrc)

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ chsh -s $(which zsh)
 ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
+ln -s $HOME/.dotfiles/gemrc $HOME/.gemrc
 
 # Install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
