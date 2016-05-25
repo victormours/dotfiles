2061ba1eed03352a8fb4ec56c5859606bc1ce47e
Victor Mours <victor.mours@gmail.com>
2016-05-25 18:41:47 +0200

    Replace development by develop in git aliases

--- git/aliases.sh
+++ git/aliases.sh
@@ -7,7 +7,7 @@ alias b='git branch -v'
 alias nb='git checkout -b' # new branch
 
 alias cm='git checkout master'
-alias gd='git checkout development'
+alias gd='git checkout develop'
 
 alias f='git add --all :/; git commit -m "fix"'
 alias fp='git add --all :/; git commit -m "fix"; git push'
@@ -41,20 +41,18 @@ alias gls='git log --graph --date=short --pretty=oneline --abbrev-commit -n10'
 alias gri='git rebase -i'
 alias grc='git rebase --continue'
 alias rim='git rebase -i master'
-alias rid='git rebase -i development'
+alias rid='git rebase -i develop'
 
 alias gclone='git clone'
 alias ginit='git init'
 
-alias gdd='git diff development'
-
 # Other git aliases
 alias gcb='git rev-parse --abbrev-ref HEAD' #current branch
-alias gdd='git diff development'
+alias gdd='git diff develop'
 alias gdm='git diff master'
 alias gi='vim .gitignore'
 alias gm='git merge --no-ff'
-alias gmd='git merge --no-ff development'
+alias gmd='git merge --no-ff develop'
 alias gmm='git merge --no-ff master'
 alias gms='git merge --squash'
 alias gam='git commit --amend --reset-author'
