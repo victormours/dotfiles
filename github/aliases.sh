ed28617d8c5ab4125e95d988c7fcc7699bd91117
Victor Mours <victor.mours@gmail.com>
2016-05-30 14:52:06 +0200

    add mpr alias

--- github/aliases.sh
+++ github/aliases.sh
@@ -1,5 +1,7 @@
 alias prs='hub browse -- pulls'
+alias mpr='hub browse -- pulls/victormours'
 alias repo='hub browse'
 
 alias prd='hub pull-request -o -b development'
 alias prm='hub pull-request -o -b master'
+
