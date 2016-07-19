f8cf74466088d5c7bbc601b257ddecaae499ca22
Victor Mours <victor.mours@gmail.com>
2016-07-19 17:58:42 +0200

    Improving aliases

--- ruby/aliases.sh
+++ ruby/aliases.sh
@@ -3,7 +3,7 @@ alias bu='bundle update'
 alias be='bundle exec'
 
 alias ras='bundle exec spring rails server'
-alias rac='bundle exec spring rails server'
+alias rac='bundle exec spring rails console'
 
 alias zr='zeus rspec'
 alias zc='zeus console'
