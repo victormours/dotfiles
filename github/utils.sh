c29bd0f8d0fca1169578bd667eb94c6f22771311
Victor Mours <victor.mours@gmail.com>
2016-08-07 10:31:18 +0200

    Add optional arguments to new_repo

--- github/utils.sh
+++ github/utils.sh
@@ -1,14 +1,17 @@
 function new_repo {
   REPO_NAME=$1
+  HUB_CREATE_ARG=$2
+
   if [ $REPO_NAME ]
   then
     mkdir $REPO_NAME
     cd $REPO_NAME
     git init
-    hub create
+    hub create $HUB_CREATE_ARG
     hub browse
   else
-    echo "usage: new_repo <my_new_repo_name>"
+    echo "usage: new_repo <my_new_repo_name> [-p]"
+    echo "The -p flag creates a private repository"
   fi
 }
 
