582f4697db139df5c6678984ff1478c619178b12
Victor Mours <victor.mours@gmail.com>
2016-08-22 12:01:56 +0200

    Remove unused alias

--- zsh/utils.sh
+++ zsh/utils.sh
@@ -9,8 +9,6 @@ alias k="osascript -e 'tell application \"Slack\" to activate'"
 
 alias ls='ls -Gh'
 
-alias "review"="open 'https://github.com/pulls?q=is%3Aopen+is%3Apr+label%3A%22Needs+Review%22+user%3Atigerlily+sort%3Acreated-asc'"
-
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 function jira {
