94bda7e6a07b28bf71bcda46693e4a2bce13b5ce
Victor Mours <victor.mours@gmail.com>
2016-09-03 17:23:27 +0200

    Move from Vundle to Plug

--- install.sh
+++ install.sh
@@ -10,18 +10,15 @@ ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
 ln -s $HOME/.dotfiles/gemrc $HOME/.gemrc
 ln -s $HOME/.dotfiles/javascript/jshintrc $HOME/.jshintrc
 
-# Install Vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +BundleInstall +q
 
 # Brew specific installs
 brew install the_silver_searcher
-brew install rbenv-bundler
 brew install autojump
 brew install git
-
-rbenv bundler on
 
 gem install hub
 gem install rubocop-git
