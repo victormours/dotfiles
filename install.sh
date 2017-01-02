cd203aad8f1f8d5370db2196d7f89a4dcf3e30d5
Victor Mours <victor.mours@gmail.com>
2017-01-02 18:08:00 +0100

    Improve install process

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ chsh -s $(which zsh)
 echo -n "Symlinking dotfiles..."
 #Symlink .zshrc and .vimrc
 ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
+mkdir $HOME/.vim/
 ln -s $HOME/.dotfiles/vim/plugin $HOME/.vim/plugin
 ln -s $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
@@ -17,7 +18,7 @@ echo 'done!'
 # Install Plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim +BundleInstall +q
+vim +PlugInstall +q
 
 # Brew specific installs
 brew install the_silver_searcher
