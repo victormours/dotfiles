56f98c1a7a643293c9fa9dd874abe10ecdd7147c
Victor Mours <victor.mours@gmail.com>
2020-12-20 18:15:21 +0100

    Update homebrew install command

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 vim +PlugInstall +q
 
 # Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle
 brew linkapps macvim
