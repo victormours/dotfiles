150018c23f7935976da5118bb4c47a41206e4e40
Victor Mours <victor.mours@gmail.com>
2024-12-28 22:15:20 +0100

    faster new pr alias

--- github/aliases.sh
+++ github/aliases.sh
@@ -5,6 +5,7 @@ alias repo='hub browse'
 alias prd='hub pull-request -o -b development'
 alias prm='hub pull-request -o -b master'
 
-alias npr='hub browse -- compare/production...`git rev-parse --abbrev-ref HEAD`'
+alias npr='use n instead'
+alias n='hub browse -- compare/production...`git rev-parse --abbrev-ref HEAD`'
 
 alias is='browse_issue'
