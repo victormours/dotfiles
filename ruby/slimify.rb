4b07821e5e0d3dd379d8ae32004b1c6801638032
Victor Mours <victor.mours@gmail.com>
2025-01-02 17:20:32 +0100

    improve slimify script

--- ruby/slimify.rb
+++ ruby/slimify.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
 ARGF.each do |line|
-  print line.gsub(/<\/[^>]*>/, '').gsub("<", "").gsub(">", "")
+  print line.gsub("<%", "").gsub("%>", "")
+    .gsub(/<\/[^>]*>/, '').gsub("<", "").gsub(">", "").gsub(/^\n$/, "")
+    .gsub(/div class='([^']+)'/, '.\1')
+    .gsub(/class='([^']+)'/, '.\1')
+    .gsub(/id='([^']+)'/, '#\1')
 end
