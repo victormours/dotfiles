240903eebab5350a0b54128e9d9e0b7053ccf095
Victor Mours <victor.mours@gmail.com>
2025-01-03 23:51:01 +0100

    better slimify

--- ruby/slimify.rb
+++ ruby/slimify.rb
@@ -1,9 +1,23 @@
 #!/usr/bin/env ruby
 
-ARGF.each do |line|
-  print line.gsub("<%", "").gsub("%>", "")
-    .gsub(/<\/[^>]*>/, '').gsub("<", "").gsub(">", "").gsub(/^\n$/, "")
-    .gsub(/div class='([^']+)'/, '.\1')
-    .gsub(/class='([^']+)'/, '.\1')
+ARGF.map do |line|
+  line.gsub("<%", "").gsub("%>", "")
+    .gsub(/<\/[^>]*>/, '').gsub("<", "").gsub(">", "")
     .gsub(/id='([^']+)'/, '#\1')
+    .gsub(/^\s+\n/, "")
+end.map do |line|
+  match = line.match(/ class=['"]([^']+)['"]/)
+  if match
+    classes = "." + match[1].gsub(" ", ".")
+    line.gsub(/ class=['"]([^']+)['"]/, classes)
+  else
+    line
+  end
+end.map do |line|
+  line.gsub(/div\./, ".")
+end.map do |line|
+  line.gsub(/ (\w+=['"]\w+['"])/, '[\1]')
+    .gsub("][", ' ')
+end.each do |line|
+  print line
 end
